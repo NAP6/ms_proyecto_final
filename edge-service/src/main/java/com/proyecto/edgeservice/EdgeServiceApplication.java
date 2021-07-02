29b145aad82f1c8f34d65cd280643731b2a7718c
Nicolas <nicolas.doo@hotmail.com>
2021-07-02 13:31:29 -0500

    probando test

---
+++ edge-service/src/main/java/com/proyecto/edgeservice/EdgeServiceApplication.java
@@ -0,0 +1,15 @@
+package com.proyecto.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class EdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServiceApplication.class, args);
+	}
+
+}
